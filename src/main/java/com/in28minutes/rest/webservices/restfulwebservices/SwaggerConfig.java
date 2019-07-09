b2e06d2b7f2ab20e6288cb3b460e37a50d376f6b
Teoh Kheng Hong <tkhenghong@gmail.com>
2019-07-09 08:23:03 +0800

    Spring Boot Actuator and HAL, until 035 Step 23 Done

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/SwaggerConfig.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/SwaggerConfig.java
@@ -1,8 +1,16 @@
 package com.in28minutes.rest.webservices.restfulwebservices;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -12,6 +20,17 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 // Enable Swagger
 @EnableSwagger2
 public class SwaggerConfig {
+
+	// Copied from ApiInfo.class
+	public static final Contact DEFAULT_CONTACT = new Contact("Ranga Karanam", "http://www.in28minutes.com",
+			"in28minutes@gmail.com");
+	public static final ApiInfo DEFAULT_API_INFO = new ApiInfo("Awesome API Title", "Awesome Api Documentation", "1.0",
+			"urn:tos", DEFAULT_CONTACT, "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0",
+			new ArrayList<VendorExtension>());
+	// Create a string that tells the devs that this APIs are accepting and
+	// producing JSON and XML
+	private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES = new HashSet<String>(
+			Arrays.asList("application/json", "application/xml"));
 
 	// This will expose a few new APIs for this SPRING
 	// It will tell the devs about:
@@ -25,7 +44,12 @@ public class SwaggerConfig {
 	@Bean
 	// Bean - Docket
 	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2);
+		// return new Docket(DocumentationType.SWAGGER_2);
+		// To give more info abou the API, add apiInfo() method:
+		// produces() to tell the devs that this APIs are accepting and producing JSON
+		// and XML
+		return new Docket(DocumentationType.SWAGGER_2).apiInfo(DEFAULT_API_INFO)
+				.produces(DEFAULT_PRODUCES_AND_CONSUMES);
 	}
 	// Swagger 2
 	// All the paths
--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
@@ -5,11 +5,16 @@ import java.util.Date;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 // Create an object to be the model of the database
 // Step 1: Create the Bean
 // Remember to auto generate all these methods using Alt + Shift + S**
 // Step 2: Create a DAO of the User. DAO is something that is created from the bean 
 // to talk to the database and get the details back.
+// Write @ApiModel to tell the devs about the validation about the User model object (Swagger)
+@ApiModel(description = "All details about the user.")
 public class User {
 // Remember to add validation of the DB Model itself
 
@@ -21,9 +26,12 @@ public class User {
 	// You can mention the limitation WITH the message when the data given is wrong
 	// like below
 	@Size(min = 2, message = "Name should have at least 2 characters.")
+	// ApiModelProperty tells the dev about some notes about this property
+	@ApiModelProperty(notes = "Name should have at least 2 characters.")
 	private String name;
 
 	@Past // This means the date must be in the past, birthday can't possibly be in future
+	@ApiModelProperty(notes = "Birth date should be in the past.")
 	private Date birthDate;
 
 	// Latest Spring doesn't show 500 Internal Server error, older version will show
