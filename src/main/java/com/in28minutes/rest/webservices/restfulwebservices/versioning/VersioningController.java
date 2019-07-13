6761133ab1eced52f074cf9f641bd920ecd2ad96
Teoh Kheng Hong <tkhenghong@gmail.com>
2019-07-13 14:53:12 +0800

    Spring Boot Security Basic Authentication 040 Step 28

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/versioning/VersioningController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/versioning/VersioningController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 //API Documentation
 //No Perfect Solution
 
+// Authentication with Spring Security
 @RestController
 public class VersioningController {
 
